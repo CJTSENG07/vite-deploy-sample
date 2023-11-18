f9c9b0779344287f52cc17b7e3aa98d5535497f6
cj07 <agassij2004@gmail.com>
2023-11-18 10:15:13 +0800

    git init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { inject } from 'vue'
+
+const axios = inject('axios')
+const url = import.meta.env.VITE_URL
+
+const getData = () => {
+  axios.get(url)
+  .then((res) => {
+    console.log(res.data)
+  })
+}
+</script>
+
+<template>
+
+
+  <button type="button" @click="getData">click me</button>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+
